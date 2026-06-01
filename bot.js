1bd648771d5c9567effbae754e03329553c1ef9e
world-pc <287334486+world-pc@users.noreply.github.com>
2026-06-01 19:32:27 -0400

    added a listen for hello command.

--- bot.js
+++ bot.js
@@ -4,6 +4,14 @@ import {Client, GatewayIntentBits} from 'discord.js';
 const client = new Client({intents: [GatewayIntentBits.Guilds,
                                      GatewayIntentBits.GuildVoiceStates]});
 
-client.once('ready', () => console.log('bot online :0'));
+client.once('ready', () => console.log('connected to server...'));
 
 client.login(process.env.DISCORD_TOKEN);
+
+client.on('interactionCreate', async interaction => {
+    if(!interaction.isChatInputCommand()) return;
+
+    if(interaction.commandName === 'hello') {
+        await interaction.reply('hi hi! <|°ᴗ°|>')
+    }
+}
