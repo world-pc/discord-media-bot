54409b84ff1a34cc6dabea75b42351fe39a582b1
kris <noneya@example.org>
2026-07-31 17:08:25 -0400

    - moved the textToSpeech function from bot.js to tt.js - implemented dotenv - added idle timeout for bot to disconnect from vc (for /play calls   thus far)
    * next todo: create a queue system for commands.

--- bot.js
+++ bot.js
@@ -1,12 +1,35 @@
 import 'dotenv/config'
+import fs from 'fs/promises'
+import os from 'os'
+import path from 'path'
+import tts from './tts.js'
+
 import {Client, GatewayIntentBits} from 'discord.js';
 import { joinVoiceChannel, createAudioPlayer, 
-    createAudioResource, AudioPlayerStatus } from '@discordjs/voice';
+    createAudioResource, AudioPlayerStatus } from '@discordjs/voice';    
+
+let idle_timeout = null; //stores the timeout object for disconnecting after some amount of idle time in vca
+let timeout_time = 15 * 60 * 1000; // 15 minutes
+function setIdleTimeout(connection) {
+    idle_timeout = setTimeout(() => {
+        connection.destroy();
+    }, timeout_time);
+}
+function clearIdleTimeout() {
+    if(idle_timeout) {
+        clearTimeout(idle_timeout);
+        idle_timeout = null;
+    }
+}
+
 
 const client = new Client({intents: [GatewayIntentBits.Guilds,
                                      GatewayIntentBits.GuildVoiceStates]});
 
 client.once('ready', () => console.log('connected to server...'));
+
+client.on('debug', console.log);
+client.on('warn', console.log);
 
 client.login(process.env.DISCORD_TOKEN);
 
@@ -16,8 +39,25 @@ client.on('interactionCreate', async interaction => {
     if(interaction.commandName === 'hello') {
         await interaction.reply('hi hi! <|°ᴗ°|>')
     }
+    else if(interaction.commandName === 'help') {
+        await interaction.reply(`available commands:\n
+                                    /hello
+                                    /play <soundname (use /soundlist for list of valid soundname>
+                                    /vc_tts <message>`);
+    }
     else if(interaction.commandName === 'soundlist') {
-        await interaction.reply("available sounds are:\n\n'hellnah'\n'bruh'\n'gasolina'");
+        const available_sounds = [
+            'hellnah',
+            'bruh',
+            'gasolina',
+            'masterchief',
+            'potatoes',
+            'doorstop',
+            'scratch',
+            'fart',
+            'archangels',
+            'fanfare'];
+        await interaction.reply(`available sounds are: \n${available_sounds.join('\n')}`);
     }
     else if(interaction.commandName === 'play') {
         const song_name = interaction.options.getString('song_name');
@@ -30,21 +70,96 @@ client.on('interactionCreate', async interaction => {
             return;
         }
         else {
-            await interaction.reply(`playing ${song_name}.wav...`);
+            try {
+                await interaction.reply(`playing ${song_name}.wav...`);
 
-            const connection = joinVoiceChannel({
-                channelId: vc.id,
-                guildId: interaction.guildId,
-                adapterCreator: interaction.guild.voiceAdapterCreator
-            });
+                const connection = joinVoiceChannel({
+                    channelId: vc.id,
+                    guildId: interaction.guildId,
+                    adapterCreator: interaction.guild.voiceAdapterCreator
+                });
 
-            const player = createAudioPlayer();
-            const resource = createAudioResource(`./sounds/${song_name}.wav`);
+                const player = createAudioPlayer();
+                const resource = createAudioResource(`./sounds/${song_name}.wav`);
 
-            player.play(resource);
-            connection.subscribe(player);
+                player.play(resource);
+                connection.subscribe(player);
 
-            //player.on(AudioPlayerStatus.Idle, () => connection.destroy());
+                player.on(AudioPlayerStatus.Playing, () => setIdleTimeout(connection));
+                player.on(AudioPlayerStatus.Idle, () => clearIdleTimeout());
+            }
+            catch(err) {
+                console.error("songs, something went wrong: ", err);
+            }
         }
     }
+    else if(interaction.commandName === 'vc_tts') {
+        const message = interaction.options.getString('message');
+
+        const member = interaction.member;
+        const vc = member.voice.channel;
+
+        if(!vc) {
+            await interaction.reply('ur not in a voice channel :(');
+            return;
+        }
+        else {
+            try {
+                await interaction.deferReply();
+                await tts.textToSpeech(message);
+            }
+            catch(err) {
+                console.error('tts failed: ', err);
+                await interaction.reply('tts failed :(');
+            }
+            finally {
+                //fs.unlink(os.tmpdir()+'message.wav', () => {});
+            }
+
+            try {
+                await interaction.editReply(`playing message.wav...`);
+
+                const connection = joinVoiceChannel({
+                    channelId: vc.id,
+                    guildId: interaction.guildId,
+                    adapterCreator: interaction.guild.voiceAdapterCreator
+                });
+
+                const player = createAudioPlayer();
+                const resource = createAudioResource(`./tts_files/message.wav`);
+
+                player.play(resource);
+                connection.subscribe(player);
+
+                //player.on(AudioPlayerStatus.Idle, () => connection.destroy());
+            }
+            catch(err) {
+                console.error("something went wrong: ", err);
+            }
+        }
+    }
+});
+
+client.on('shardDisconnect', (event, shard_id) => {
+    console.log(`shard {shard_id} disconnected. code: {event.code}, reason: '{event.reason}'`);
+});
+
+client.on('shardReconnecting', (shard_id) => {
+    console.log(`shard {shard_id} reconnecting...`);
+});
+
+client.on('shardResume', (shard_id, replayed_events) => {
+    console.log(`resuming shard {shard_id}. replaying events {replayed_events}`);
+});
+
+client.on('shardError', (error, shard_id) => {
+    console.error(`shard {shard_id} error: `, error);
+});
+
+process.on('uncaughtException', (err) => {
+    console.error('uncaught exception: ', err);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+    console.error('unhandled rejection at ', promise, ' reason: ', reason);
 });
---
+++ tts.js
@@ -0,0 +1,33 @@
+import {execFile} from 'child_process'
+import {promisify} from 'util'
+import fs from 'fs/promises'
+
+const execFileAsync = promisify(execFile);
+
+async function textToSpeech(message) {
+    
+    await fs.writeFile('./tts_files/msg.txt', message);
+
+    try {
+        
+        const {stdout, stderr} = await execFileAsync(
+            './venv/bin/piper',
+            ['--model', './venv/piper_voices/en_GB-southern_english_female-low.onnx',
+             '--input_file', './tts_files/msg.txt',
+             '--output_file', './tts_files/message.wav']
+        );
+
+        console.log(stdout);
+        console.log(stderr);
+    }
+
+    catch(err) {
+        console.error('exit code:', err.code);
+        console.error('stderr:', err.stderr);
+        console.error('stdout:', err.stdout);
+    }
+
+    //fs.unlink('./tts_files/msg.txt');
+}
+
+export default {textToSpeech};
